3a200525b7ef5d1e18e108a0d2d6f610b1d6de17
vipul-dixit-2004 <dixit.vipul0511@gmail.com>
2025-07-09 20:52:03 +0530

    trying to deploy

--- run.py
+++ run.py
@@ -4,5 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('FLASK_RUN_PORT', 5000))
-    app.run(port=port)
+    port = int(os.environ.get('PORT', 5000))  # Use 'PORT', not 'FLASK_RUN_PORT' for Render
+    app.run(host='0.0.0.0', port=port)
+
