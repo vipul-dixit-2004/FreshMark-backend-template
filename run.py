1bf97efd285a69f573a96099d823db0cef76e89e
vipul-dixit-2004 <dixit.vipul0511@gmail.com>
2025-07-09 19:57:05 +0530

    trying to deploy

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('FLASK_RUN_PORT', 5000))
+    app.run(port=port)
